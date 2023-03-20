bbb325127fb06d0141cc2c7ae5a2cbc0719fb41b
5ENP4I <syzonenkod@gmail.com>
2023-03-20 14:56:28 +0100

    dividedWork

--- src/App.vue
+++ src/App.vue
@@ -3,28 +3,23 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="flex">
+    <div class="flex-auto bg-red-500 justify-between text-center">
+      Pavel
+    </div>
+    <div class="flex-auto bg-violet-500">
+      <div class="flex-auto bg-blue-500 justify-between text-center">
+        Dimi
+      </div>
+      <div class="flex-auto bg-red-500 justify-between text-center">
+        Pavel
+      </div>
+    </div>
+    <div class="flex-auto bg-blue-500 justify-between text-center">
+      Dimi
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  </div>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
 </style>

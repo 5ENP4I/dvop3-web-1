b640d9d6636a4740ecf5e43e9d6cbf02e935292a
Vaše Jméno <vas@email.com>
2023-04-17 16:42:28 +0200

    leftSideBar

--- src/components/LeftSidebar.vue
+++ src/components/LeftSidebar.vue
@@ -1,41 +1,45 @@
 <script setup lang="ts">
+interface Data {
+  elements: { title: string, image: string }[]
+}
+
+const data: Data = {
+  elements: [
+    { title: 'Map', image: '/map.png' },
+    { title: 'Saved Location', image: '/saved-location.png' },
+    { title: 'Calendar', image: '/calendar.png' },
+    { title: 'Settings', image: '/settings.png' }
+  ]
+}
 </script>
 
 <template>
-  <div class="font-extrabold text-xl pt-10 pb-16 border-r-2">
-    <img class="inline pr-2" src="/cuacane-logo.png">
-    Cuacane
-  </div>
-  <div class="pb-7"></div>
+  <div class="flex flex-row">
+    <div class="basis-full text-left pl-20">
+      <div class="font-extrabold text-xl py-20 border-r-2">
+        <img class="inline w-10" src="/cuacane-logo.png">
+        Cuacane
+      </div>
 
-  <div class="font-semibold pr-6 py-3 border-r-2 border-black">
-    <img class="inline w-8 pb-1 pr-2" src="/dashboard-logo.png">
-    Dashboard
-  </div>
+      <div class="font-semibold border-r-2 border-black h-42">
+        <img class="inline w-8 pb-1" src="/dashboard-logo.png">
+        Dashboard
+      </div>
 
-  <div class="pt-7"></div>
-  <div class="font-jakarta inline-block pr-20 border-r-2 float-right">
-    <div class="pr-14">
-      <img class="inline w-8 pb-1 pr-2" src="/map.png">
-      Map
-    </div>
-    <div class="pl-6 pt-12">
-      <img class="inline w-8 pb-1 pr-2" src="/saved-location.png">
-      Saved Location
-    </div>
-    <div class="pr-6 pt-12">
-      <img class="inline w-8 pb-1 pr-2" src="/calendar.png">
-      Calendar
-    </div>
-    <div class="pr-8 pt-12">
-      <img class="inline w-8 pb-1 pr-2" src="/settings.png">
-      Settings
-    </div>
+      <div class="font-jakarta border-r-2">
+        <div v-for="(element, index) in data.elements" :key="index">
+          <div class=" pt-16">
+            <img class="inline w-8 pb-1" :src="element.image">
+            {{ element.title }}
+          </div>
+        </div>
 
-    <div class="pr-8 pt-96">
-      <img class="inline w-8 pb-1 pr-2" src="/logout.png">
-      Log Out
+        <div class="pt-96">
+          <img class="inline w-8 pb-1" src="/logout.png">
+          Log Out
+        </div>
+        <div class="pt-8"></div>
+      </div>
     </div>
-    <div class="pt-6"></div>
   </div>
 </template>
